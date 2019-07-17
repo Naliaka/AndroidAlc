58ee4c66eaa5d207745bec2e4f1e085382fb8b0d
Naliaka <milkawafula@gmail.com>
2019-07-17 11:19:32 +0400

    first commit

---
+++ ChallangeApplication/app/src/main/java/com/example/user_pc/challangeapplication/profile.java
@@ -0,0 +1,16 @@
+package com.example.user_pc.challangeapplication;
+
+package com.wordpress.eschoolsl.alc4phase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
